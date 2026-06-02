007383b15ea221c57e140cbff4267b214c7db432
iljmkruijt-rgb <iljmkruijt@gmail.com>
2026-06-02 22:47:44 +0200

    Files uit VS in git gezet

---
+++ script.js
@@ -0,0 +1,13 @@
+
+window.showPage = function(id) {
+    document.querySelectorAll(".page").forEach(p => {
+        p.classList.remove("active");
+    });
+
+    document.getElementById(id).classList.add("active");
+}
+
+showPage("home");
+
+
+
